17e27c93d0f183658c4cbc41246266f97d1f27ef
JuliansRecent <juliansrecent@gmail.com>
2023-01-18 21:22:28 -0800

    Module complete. Print a prompt out in the console asking for the user's name. Store the name in a string variable, then print 'Hi ' + their inputted name from the console.

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -7,6 +7,13 @@ public class Greeting {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        // Print to the console "What's your name?"
+        System.out.println("What's your name?");
 
+        // Store the users input to the string variable 'inputted name'
+        String inputted_name = scanner.nextLine();
+
+        // Print out "Hi " then the users inputted name
+        System.out.println("Hi " + inputted_name);
     }
 }
